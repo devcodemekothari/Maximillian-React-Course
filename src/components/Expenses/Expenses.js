4f5e309ff5aad72011c4a6dba4e80adc689ce607
Dev Kothari <thisisdevkothari@gmail.com>
2022-01-04 17:19:11 +0530

    Adding chart to expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from './ExpensesChart';
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -15,9 +15,7 @@ const Expenses = (props) => {
 
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
-  });
-
-  let expensesContent = <p>No expenses found.</p>;
+  });  
 
   return (
     <Card className="expenses">
@@ -25,6 +23,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
